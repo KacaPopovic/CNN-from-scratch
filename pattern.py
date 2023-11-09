58b2a4a3a07c2d8c0f987d725469e09a20d372c8
Katarina Popovic <kaca.popovic000@gmail.com>
2023-11-09 18:45:22 +0100

    Update pattern.py
    Added docstring to pattern.py Classes

--- pattern.py
+++ pattern.py
@@ -3,13 +3,50 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-class Checker():
+class Checker:
+
+    """
+    A class for generating and displaying a checkerboard pattern.
+
+    Args:
+        resolution (int): The resolution of the checkerboard pattern.
+        tile_size (int): The size of each individual tile in the checkerboard.
+
+    Attributes:
+        resolution (int): The resolution of the checkerboard pattern.
+        tile_size (int): The size of each individual tile in the checkerboard.
+        output (numpy.ndarray): The generated checkerboard pattern.
+
+    Methods:
+        draw():
+            Generates a checkerboard pattern based on the provided resolution and tile size.
+
+        show():
+            Displays the generated checkerboard pattern using matplotlib.
+
+    Example:
+        # Initialize Checker object with resolution 800 and tile size 20
+        checkerboard = Checker(resolution=800, tile_size=20)
+        # Generate the checkerboard pattern
+        checkerboard.draw()
+        # Display the checkerboard pattern
+        checkerboard.show()
+    """
     def __init__(self, resolution, tile_size):
         self.resolution = resolution
         self.tile_size = tile_size
         self.output = None
 
     def draw(self):
+        """
+        Generates a checkerboard pattern based on the provided resolution and tile size.
+
+        Returns:
+            numpy.ndarray: The generated checkerboard pattern.
+
+        Raises:
+            None
+        """
         black_tiles = np.zeros(shape=(self.tile_size, self.tile_size))
         white_tiles = np.ones(shape=(self.tile_size, self.tile_size))
         tile = np.concatenate((black_tiles, white_tiles), axis=0)
@@ -22,13 +59,53 @@ class Checker():
         return copy.deepcopy(tile)
 
     def show(self):
+        """
+        Displays the generated checkerboard pattern using matplotlib.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         plt.figure()
         plt.imshow(self.output, cmap='gray')
         plt.title('Checkerboard')
         plt.show()
 
 
-class Circle():
+class Circle:
+
+    """
+    A class for generating and displaying a filled circle on a grid.
+
+    Args:
+        resolution (int): The resolution of the grid (both width and height).
+        radius (float): The radius of the circle.
+        position (tuple): The position (x, y) of the center of the circle in the grid.
+
+    Attributes:
+        resolution (int): The resolution of the grid (both width and height).
+        radius (float): The radius of the circle.
+        position (tuple): The position (x, y) of the center of the circle in the grid.
+        output (numpy.ndarray): The grid with the filled circle drawn on it.
+
+    Methods:
+        draw():
+            Draws a filled circle on the grid based on the provided radius and position.
+
+        show():
+            Displays the grid with the filled circle using matplotlib.
+
+    Example:
+        # Initialize Circle object with resolution 800, radius 50, and position (400, 400)
+        circle = Circle(resolution=800, radius=50, position=(400, 400))
+        # Draw the filled circle on the grid
+        circle.draw()
+        # Display the grid with the filled circle
+        circle.show()
+
+    """
     def __init__(self, resolution, radius, position):
         self.resolution = resolution
         self.radius = radius
@@ -36,6 +113,15 @@ class Circle():
         self.output = None
 
     def draw(self):
+        """
+        Draws a filled circle on the grid based on the provided radius and position.
+
+        Returns:
+            numpy.ndarray: The grid with the filled circle drawn on it.
+
+        Raises:
+            None
+        """
         self.output = np.zeros(shape=(self.resolution, self.resolution))
 
         x_grid, y_grid = np.meshgrid(np.arange(self.resolution), np.arange(self.resolution))
@@ -48,18 +134,62 @@ class Circle():
         return copy.deepcopy(self.output)
 
     def show(self):
+        """
+        Displays the grid with the filled circle using matplotlib.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         plt.figure()
         plt.imshow(self.output, cmap='gray')
         plt.title('Circle')
         plt.show()
 
 
-class Spectrum():
+class Spectrum:
+
+    """
+    A class for generating and displaying a color spectrum image.
+
+    Args:
+        resolution (int): The resolution of the spectrum image (both width and height).
+
+    Attributes:
+        resolution (int): The resolution of the spectrum image (both width and height).
+        output (numpy.ndarray): The color spectrum image represented as a NumPy array.
+
+    Methods:
+        draw():
+            Generates a color spectrum image based on the provided resolution.
+
+        show():
+            Displays the color spectrum image using matplotlib.
+
+    Example:
+        # Initialize Spectrum object with resolution 800
+        spectrum = Spectrum(resolution=800)
+        # Generate the color spectrum image
+        spectrum.draw()
+        # Display the color spectrum image
+        spectrum.show()
+    """
     def __init__(self, resolution):
         self.resolution = resolution
         self.output = None
 
     def draw(self):
+        """
+        Generates a color spectrum image based on the provided resolution.
+
+        Returns:
+            numpy.ndarray: The color spectrum image represented as a NumPy array.
+
+        Raises:
+            None
+        """
         self.output = np.zeros(shape=(self.resolution, self.resolution, 3))
         # CH0 -> RED, CH1 -> GREEN, CH3 -> BLUE
 
@@ -75,6 +205,16 @@ class Spectrum():
         return copy.deepcopy(self.output)
 
     def show(self):
+
+        """
+        Displays the color spectrum image using matplotlib.
+
+        Returns:
+            None
+
+        Raises:
+            None
+        """
         plt.figure()
         plt.imshow(self.output)
         plt.title('Spectrum')
