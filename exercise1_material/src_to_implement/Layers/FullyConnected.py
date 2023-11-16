Fix bugs in NeuralNetwork.py and SoftMax.py

exercise1_material/src_to_implement/Layers/FullyConnected.py
@@ -6,21 +6,22 @@ class FullyConnected(BaseLayer):
     def __init__(self, input_size, output_size):
         super().__init__()
         self.trainable = True
-        self.weights = np.random.randn(input_size, output_size)
+        self.weights = np.random.rand(input_size + 1, output_size)
         self._optimizer = None
         self.input_tensor = None
         self.gradient_tensor = None
 
     def forward(self, input_tensor):
-        self.input_tensor = input_tensor
-        output_tensor = np.dot(input_tensor, self.weights)
+        bias = np.ones(shape=(input_tensor.shape[0],1))
+        self.input_tensor = np.append(input_tensor, bias, axis=1)
+        output_tensor = np.dot(self.input_tensor, self.weights)
         return output_tensor
 
     def backward(self, error_tensor):
-        previous_error_tensor = np.dot(error_tensor,self.weights.transpose())
+        previous_error_tensor = np.dot(error_tensor,self.weights[:-1,:].T)
+        gradient_tensor = np.dot(self.input_tensor.transpose(), error_tensor)
+        self.gradient_tensor = gradient_tensor
         if self.optimizer:
-            gradient_tensor = np.dot(self.input_tensor.transpose(), error_tensor)
-            self.gradient_tensor = gradient_tensor
             updated_weight_tensor = self.optimizer.calculate_update(self.weights, gradient_tensor)
             self.weights = updated_weight_tensor
         return previous_error_tensor

exercise1_material/src_to_implement/Layers/SoftMax.py
@@ -19,14 +19,6 @@ class SoftMax(BaseLayer):
     def backward(self, error_tensor):
 
         # Todo check if this makes sense
-        #temp = np.diagflat(self.output_tensor) - self.output_tensor.T
-        #jacobian_matrix = np.dot(self.output_tensor,temp)
-        enum = np.arange(error_tensor.shape[0])
-        z= np.zeros_like(error_tensor)
-
-        for i in enum:
-            x=error_tensor[i,:]
-            y= self.output_tensor[i,:].T
-            z[i,:] = x*y
-        prev_error_tensor = self.output_tensor * (error_tensor - z)
-        return prev_error_tensor
+        inner_sum = np.sum(error_tensor*self.output_tensor, axis=1, keepdims=True)
+        previous_error_tensor = self.output_tensor * (error_tensor-inner_sum)
+        return previous_error_tensor

exercise1_material/src_to_implement/NeuralNetwork.py
@@ -1,33 +1,39 @@
 import copy
 
+import numpy as np
+
+
 class NeuralNetwork:
     def __init__(self, optimizer):
         self.optimizer = optimizer
-        self.loss = None
-        self.layers = None
+        self.loss = []
+        self.layers = []
         self.data_layer = None
         self.loss_layer = None
         self.label_tensor = None
 
     def forward(self):
+
         input_tensor, label_tensor = self.data_layer.next()
         self.label_tensor = label_tensor
-        for i in range(self.layers.len()-1):
+        for i in range(len(self.layers)):
             input_tensor = self.layers[i].forward(input_tensor)
-        loss = self.layers[-1].forward(input_tensor, label_tensor)
+        #loss = self.layers[-1].forward(input_tensor, label_tensor)
         output_tensor = input_tensor
-        return loss
+        predicted_class = np.argmax(output_tensor)
+        # todo maybe wrong
+        return predicted_class
 
     def backward(self):
         error_tensor = self.layers[-1].backward(self.label_tensor)
-        for i in range(self.layers.len()-2,-1):
+        for i in range(self.layers.len()-2, -1):
             error_tensor = self.layers[i].backward(error_tensor)
         return error_tensor
 
     def append_layer(self, layer):
         if layer.trainable:
-            optimizor_copy = copy.deepcopy(self.optimizer)
-            layer.optimizer(optimizor_copy)
+            optimizer_copy = copy.deepcopy(self.optimizer)
+            layer.optimizer = optimizer_copy
         self.layers.append(layer)
 
     def train(self, iterations):
@@ -36,9 +42,8 @@ class NeuralNetwork:
             self.loss.append(loss)
 
     def test(self, input_tensor):
-        for i in range(self.layers.len()-1):
+        for i in range(len(self.layers)-1):
             input_tensor = self.layers[i].forward(input_tensor)
         probabilities = input_tensor
         return probabilities
-
 