7cbcf137003c02f86f303245233762284e3059c9
Katarina Popovic <kaca.popovic000@gmail.com>
2023-12-09 12:50:49 +0100

    Created constant, uniform, Xavier and He initializers

---
+++ exercise1_material/src_to_implement/Layers/Initializers.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+
+class Constant:
+    def __init__(self, initialization_value=0.1):
+        self.initialization_value = initialization_value
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        init_weights = np.ones_like(weights_shape)*self.initialization_value
+        return init_weights
+
+
+class UniformRandom:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        init_weights = np.random.uniform(0, 1, weights_shape)
+        return init_weights
+
+
+class Xavier:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        std = np.sqrt(1/np.mean(fan_in, fan_out))
+        init_weights = np.random.normal(0, std, weights_shape)
+        return init_weights
+
+
+class He:
+    def __init__(self):
+        pass
+
+    def initialize(self, weights_shape, fan_in, fan_out):
+        std = np.sqrt(2 / fan_in)
+        init_weights = np.random.normal(0, std, weights_shape)
+        return init_weights
+    
