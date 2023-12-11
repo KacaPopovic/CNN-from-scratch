4c7ee1ada5b8e86556bb6e8fa62c3bb448e8a842
Katarina Popovic <kaca.popovic000@gmail.com>
2023-12-12 00:58:52 +0100

    Change backward in pooling

--- exercise1_material/src_to_implement/Layers/Pooling.py
+++ exercise1_material/src_to_implement/Layers/Pooling.py
@@ -32,43 +32,6 @@ class Pooling(BaseLayer):
 
         return output_tensor
 
-    def backward(self, error_tensor):
-        prev_error_tensor = self.max_locations
-        error_tensor_flatten = error_tensor.flatten()
-        batch, channel, y, x = prev_error_tensor.shape
-        counter = 0
-        for b in range(batch):
-            for c in range(channel):
-                for i in range(y):
-                    for j in range(x):
-                        if prev_error_tensor[b, c, i, j] == 1:
-                            prev_error_tensor[b, c, i, j] *= error_tensor_flatten[counter]
-                            counter += 1
-        return prev_error_tensor
-
-    def backward2(self, error_tensor):
-        batch, channel, y, x = self.input_tensor.shape
-        prev_error_tensor = np.zeros_like(self.input_tensor)
-
-        for b in range(batch):
-            for c in range(channel):
-                for i in range(error_tensor.shape[2]):
-                    for j in range(error_tensor.shape[3]):
-                        start_y = i * self.stride[0]
-                        end_y = start_y + self.pooling_shape[0]
-                        start_x = j * self.stride[1]
-                        end_x = start_x + self.pooling_shape[1]
-
-                        # Find the location of the max value in the original pooling region
-                        max_pos = np.where(self.max_locations[b, c, start_y:end_y, start_x:end_x])
-
-                        # Propagate the error to the location of the max value
-                        if len(max_pos[0]) > 0 and len(max_pos[1]) > 0:
-                            prev_error_tensor[b, c, start_y + max_pos[0][0], start_x + max_pos[1][0]] = error_tensor[
-                                b, c, i, j]
-
-        return prev_error_tensor
-
     def backward1(self, error_tensor):
         batch, channel, y, x = self.input_tensor.shape
         prev_error_tensor = np.zeros_like(self.input_tensor)
@@ -92,5 +55,37 @@ class Pooling(BaseLayer):
                             prev_error_tensor[b, c, y_max, x_max] += error_tensor[b, c, i, j]
 
         return prev_error_tensor
+
+    def backward(self, error_tensor):
+        batch, channel, y, x = self.input_tensor.shape
+        prev_error_tensor = np.zeros_like(self.input_tensor)
+
+        for b in range(batch):
+            for c in range(channel):
+                for i in range(error_tensor.shape[2]):
+                    for j in range(error_tensor.shape[3]):
+                        start_y = i * self.stride[0]
+                        end_y = start_y + self.pooling_shape[0]
+                        start_x = j * self.stride[1]
+                        end_x = start_x + self.pooling_shape[1]
+
+                        # Ensure we don't go past the edge of the input tensor
+                        end_y = min(end_y, y)
+                        end_x = min(end_x, x)
+
+                        # Find the location of the max value in the original pooling region
+                        max_pos = np.where(self.max_locations[b, c, start_y:end_y, start_x:end_x])
+
+                        # Check if there is at least one max location
+                        if max_pos[0].size > 0:
+                            # Take the first occurrence in row-wise order
+                            y_max = start_y + max_pos[0][0]
+                            x_max = start_x + max_pos[1][0]
+
+                            # Propagate the error to the location of the max value
+                            prev_error_tensor[b, c, y_max, x_max] += error_tensor[b, c, i, j]
+
+        return prev_error_tensor
+
 
 
